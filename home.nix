d7c77e979f24a09f8cc0e17de38bc1e36f84aacb
Naysayer <naysayer@hey.com>
2026-01-13 00:18:06 +0700

    add lazyvim to nix

--- home.nix
+++ home.nix
@@ -139,23 +139,61 @@ in
       let mapleader = " "
       let maplocalleader = "\\"
 
-      colorscheme catppuccin
+      lua << EOF
+      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+      if not (vim.uv or vim.loop).fs_stat(lazypath) then
+        vim.fn.system({
+          "git",
+          "clone",
+          "--filter=blob:none",
+          "--branch=stable",
+          "https://github.com/folke/lazy.nvim.git",
+          lazypath,
+        })
+      end
+      vim.opt.rtp:prepend(lazypath)
+
+      require("lazy").setup({
+        spec = {
+          { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+          { import = "lazyvim.plugins.extras.coding.mini-surround" },
+          { import = "lazyvim.plugins.extras.editor.mini-files" },
+          { import = "lazyvim.plugins.extras.editor.telescope" },
+        },
+        defaults = {
+          lazy = false,
+          version = false,
+        },
+        install = {
+          colorscheme = { "catppuccin" },
+        },
+        performance = {
+          rtp = {
+            disabled_plugins = {
+              "gzip",
+              "tarPlugin",
+              "tohtml",
+              "tutor",
+              "zipPlugin",
+            },
+          },
+        },
+      })
+      EOF
     '';
     plugins = with pkgs.vimPlugins; [
+      catppuccin-nvim
       lazy-nvim
-      nvim-treesitter
-      telescope-nvim
-      telescope-fzf-native-nvim
-      which-key-nvim
-      nvim-autopairs
-      nvim-cmp
-      cmp-nvim-lsp
-      cmp-path
-      cmp-buffer
-      nvim-lspconfig
-      gitsigns-nvim
-      nvim-comment
-      indent-blankline-nvim
+    ];
+    extraPackages = with pkgs; [
+      git
+      ripgrep
+      fd
+      fzf
+      nixfmt
+      stylua
+      prettier
+      ruff
     ];
   };
 
