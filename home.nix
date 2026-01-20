aae8dcaefb226ddf68387599aa102bdc7f122e23
Naysayer <naysayer@hey.com>
2026-01-20 17:11:43 +0700

    update nvim config

--- home.nix
+++ home.nix
@@ -119,18 +119,6 @@ in
   };
 
   xdg.configFile = {
-    "nvim/init.lua".text = ''
-      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-      if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-          "git", "clone", "--filter=blob:none",
-          "https://github.com/folke/lazy.nvim.git",
-          "--branch=stable", lazypath
-        })
-      end
-      vim.opt.rtp:prepend(lazypath)
-      require("lazy").setup("plugins")
-    '';
-    "nvim/lua/plugins".source = ./nvim/lua/plugins;
+    "nvim".source = ./nvim;
   };
 }
