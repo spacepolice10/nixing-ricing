f8bb28d46588a39400f13a49baf71c3cc0e02ae1
Naysayer <naysayer@hey.com>
2026-03-11 23:26:19 +0700

    set up diffstatic in git

--- home.nix
+++ home.nix
@@ -36,6 +36,7 @@
       unfocused-split-opacity = 0.88;
       split-divider-color = "#222222";
       window-decoration = "none";
+      window-padding-balance = false;
       macos-option-as-alt = true;
       keybind = [
         "ctrl+cmd+h=goto_split:left"
@@ -293,6 +294,7 @@
     settings = {
       user.name = "spacepolice10";
       user.email = "naysayer@hey.com";
+      diff.external = "difft";
     };
   };
 
@@ -314,7 +316,26 @@
   programs.lazygit = {
     enable = true;
     settings = {
-      gui.theme.name = "dracula";
+      gui.theme = {
+        activeBorderColor = [
+          "#FF79C6"
+          "bold"
+        ];
+        inactiveBorderColor = [ "#BD93F9" ];
+        searchingActiveBorderColor = [
+          "#8BE9FD"
+          "bold"
+        ];
+        optionsTextColor = [ "#6272A4" ];
+        selectedLineBgColor = [ "#6272A4" ];
+        inactiveViewSelectedLineBgColor = [ "bold" ];
+        cherryPickedCommitFgColor = [ "#6272A4" ];
+        cherryPickedCommitBgColor = [ "#8BE9FD" ];
+        markedBaseCommitFgColor = [ "#8BE9FD" ];
+        markedBaseCommitBgColor = [ "#F1FA8C" ];
+        unstagedChangesColor = [ "#FF5555" ];
+        defaultFgColor = [ "#F8F8F2" ];
+      };
       git.paging = {
         colorArg = "always";
         externalDiffCommand = "difft --color=always";
